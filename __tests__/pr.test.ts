559e52aa34f69d8b80a7bf7fcc8001df74e8366b
Harry Gogonis <harry@supplypike.com>
2021-06-03 14:43:43 -0400

    feat: option to ignore PR authors | SRENEW-630 (#9)

--- __tests__/pr.test.ts
+++ __tests__/pr.test.ts
@@ -1,34 +1,44 @@
 import {PullRequestEvent, PushEvent} from '@octokit/webhooks-definitions/schema'
 import {validate} from '../src/pr'
 import {pr} from '../src/mock/pull_request_mock'
+import {Options} from '../src/options'
 
-const project = 'SRENEW'
+let options: Options
 let mock: PullRequestEvent
 
 beforeEach(() => {
   mock = JSON.parse(JSON.stringify(pr))
+
+  options = {project: 'SRENEW', ignoreAuthor: []}
 })
 
 test('invalid PR', async () => {
-  expect(validate(pr, project)).toEqual(false)
+  expect(validate(pr, options)).toEqual(false)
 })
 
 test('valid PR title', async () => {
   mock.pull_request.title =
     'Update the README with new information | SRENEW-1234'
 
-  expect(validate(mock, project)).toEqual(true)
+  expect(validate(mock, options)).toEqual(true)
 })
 
 test('valid PR branch', async () => {
   mock.pull_request.head.ref = 'foo-SRENEW-1234'
 
-  expect(validate(mock, project)).toEqual(true)
+  expect(validate(mock, options)).toEqual(true)
 })
 
-test('works with regex project', async () => {
+test('works with regex options', async () => {
   mock.pull_request.head.ref = 'foo-FOO-1234'
-  const reProject = `[FOO|SRENEW]`
+  options.project = '[SRENEW|FOO]'
 
-  expect(validate(mock, reProject)).toEqual(true)
+  expect(validate(mock, options)).toEqual(true)
+})
+
+test('valid if ignoreAuthor matches', async () => {
+  options.ignoreAuthor = ['dependabot']
+  mock.pull_request.user.login = 'dependabot'
+
+  expect(validate(mock, options)).toEqual(true)
 })
