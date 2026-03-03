0dff41ff624a9a38db3922681da57357df3ad24c
Harry Gogonis <harry@supplypike.com>
2026-03-03 13:59:57 -0500

    [SRENEW-3365] fail action on caught exception (#515)
    * setFailed on caught error
    
    * npm audit fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ export async function main(): Promise<void> {
   try {
     await process(github.context)
   } catch (error) {
-    core.error(`Error caught in main: ${error}`)
+    core.setFailed(`Action failed with error ${error}`)
   }
 }
 
